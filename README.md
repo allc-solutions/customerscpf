acc9aa66341b40b894cec08b2b4ec0bd9ecf9cbb
allc <andrellopes@live.com>
2022-08-30 13:35:14 -0300

    first

---
+++ README.md
@@ -0,0 +1,16 @@
+# Módulo CPF para loja Prestashop 1.7.x
+
+Este módulo adiciona os campos CPF/CNPJ e RG/Inscrição estadual no cadastro tabela Customers e modulos_cpf e retorna essas informações pela API custormes.
+
+### Características
+- O módulo faz a validação do CPF/CNPJ.
+- Verifica se o CPF/CNPJ já existe no banco de dados.
+
+### Instalação
+Efetue o download do módulo:
+[Link para download do módulo cpf](https://github.com/Andrellopes/customerscpf/archive/refs/tags/2.0.0.zip)
+
+Acesse a área administrativa e vá em Módulos => Módulos e Serviços.
+Clique no botão "Enviar um módulo". Selecione o arquivo que você baixou no link acima e pronto! Não precisa fazer configurações adicionais.
+
+Esse projeto utiliza como base o https://github.com/edersondev/psmodcpf/
---
+++ customerscpf.php
@@ -0,0 +1,455 @@
+<?php
+/**
+ * 2007-2018 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ *  @author    PrestaShop SA <contact@prestashop.com>
+ *  @copyright 2007-2018 PrestaShop SA
+ *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ *  International Registered Trademark & Property of PrestaShop SA
+ */
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+include dirname(__FILE__) . '/classes/ValidateDocumento.php';
+
+use PrestaShop\Module\Customerscpf\ValidateDocumento;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class Customerscpf extends Module
+{
+    protected $config_form = false;
+
+    public $mensagemError = 'Número inválido. Verifique por favor!';
+
+    public function __construct()
+    {
+        $this->name = 'customerscpf';
+        $this->tab = 'front_office_features';
+        $this->version = '2.0.1';
+        $this->author = 'Andre Lopes / Ederson Ferreira';
+        $this->need_instance = 0;
+
+        /**
+         * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
+         */
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Módulo CPF');
+        $this->description = $this->l('Adiciona o campo CPF / CNPJ no cadastro do cliente');
+
+        $this->confirmUninstall = $this->l('Tem certeza de que deseja desinstalar o módulo CPF?');
+
+        $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
+    }
+
+    public function install()
+    {
+        Configuration::updateValue('CUSTOMERSCPF_LIVE_MODE', false);
+
+        include dirname(__FILE__) . '/sql/install.php';
+
+        $this->alteraTabela();
+
+        return parent::install() &&
+        $this->registerHook('actionFrontControllerSetMedia') &&
+        $this->registerHook('actionAdminControllerSetMedia') &&
+        $this->registerHook('validateCustomerFormFields') &&
+        $this->registerHook('actionCustomerAccountAdd') &&
+        $this->registerHook('actionCustomerAccountUpdate') &&
+        $this->registerHook('actionAdminCustomersFormModifier') &&
+        $this->registerHook('actionAdminCustomersControllerSaveBefore') &&
+        $this->registerHook('actionAdminCustomersControllerSaveAfter') &&
+        $this->registerHook('additionalCustomerFormFields') &&
+        $this->registerHook('actionCustomerFormBuilderModifier');
+    }
+
+    public function uninstall()
+    {
+        Configuration::deleteByName('CUSTOMERSCPF_LIVE_MODE');
+
+        include dirname(__FILE__) . '/sql/uninstall.php';
+
+        // não limpar dados ao desinstalar
+        //$Query =   "ALTER TABLE "._DB_PREFIX_."customer DROP COLUMN tipo;";
+        //if (!Db::getInstance()->Execute($Query)) {
+        //    return false;
+        //}
+        //$Query =   "ALTER TABLE "._DB_PREFIX_."customer DROP COLUMN cpf_cnpj;";
+        //if (!Db::getInstance()->Execute($Query)) {
+        //    return false;
+        //}
+        //$Query =   "ALTER TABLE "._DB_PREFIX_."customer DROP COLUMN rg_ie;";
+        //if (!Db::getInstance()->Execute($Query)) {
+        //    return false;
+        //}
+
+        return parent::uninstall();
+    }
+
+    /**
+     * Load the configuration form
+     */
+    public function getContent()
+    {
+        $this->context->smarty->assign('module_dir', $this->_path);
+
+        $output = $this->context->smarty->fetch($this->local_path . 'views/templates/admin/configure.tpl');
+
+        return $output;
+    }
+
+    /**
+     * Add the CSS & JavaScript files you want to be loaded in the BO.
+     */
+    public function hookActionAdminControllerSetMedia()
+    {
+        if (Tools::getValue('module_name') == $this->name) {
+            $this->context->controller->addCSS($this->_path . 'views/css/back.css');
+        }
+
+        if (Tools::getValue('controller') == 'AdminCustomers') {
+            $this->context->controller->addJS($this->_path . 'views/js/jquery.mask.min.js');
+            $this->context->controller->addJS($this->_path . 'views/js/back.js');
+        }
+    }
+
+    /**
+     * Add the CSS & JavaScript files you want to be added on the FO.
+     */
+    public function hookActionFrontControllerSetMedia()
+    {
+        if (Tools::getValue('controller') == 'order' || Tools::getValue('controller') == 'identity' || (Tools::getValue('controller') == 'authentication' && Tools::getValue('create_account') == '1')) {
+            $this->context->controller->registerJavascript(
+                'module-customerscpf-jquerymask',
+                'modules/' . $this->name . '/views/js/jquery.mask.min.js',
+                ['priority' => 210]
+            );
+            $this->context->controller->registerJavascript(
+                'module-customerscpf-front',
+                'modules/' . $this->name . '/views/js/front.js',
+                ['priority' => 211]
+            );
+        }
+    }
+
+    public function hookValidateCustomerFormFields($params)
+    {
+        foreach ($params['fields'] as $field) {
+            if ($field->getName() == 'documento') {
+                $objValidateDoc = new ValidateDocumento();
+                if (!$objValidateDoc->validarDocumento($field->getValue())) {
+                    $field->addError($this->mensagemError);
+                }
+                $id_customer = (is_null($this->context->customer->id) ? 0 : (int) $this->context->customer->id);
+                if ($this->checkDuplicate($field->getValue(), $id_customer) !== false) {
+                    $field->addError('O documento informado já está cadastrado!');
+                }
+            }
+        }
+    }
+
+    public function hookActionCustomerAccountAdd($params)
+    {
+        $this->insertDocumento($params['newCustomer']->id);
+    }
+
+    public function hookActionCustomerAccountUpdate($params)
+    {
+        $result = $this->searchCustomer((int) $params['customer']->id);
+        if ($result === false) {
+            $this->insertDocumento($params['customer']->id);
+        }
+    }
+
+    public function hookActionCustomerFormBuilderModifier($params)
+    {
+        /** @var FormBuilderInterface $formBuilder */
+        $formBuilder = $params['form_builder'];
+        if ($params['route'] === 'admin_customers_edit') {
+            $formBuilder->add('tp_documento', ChoiceType::class, [
+                'choices' => ['CPF' => '1', 'CNPJ' => '2'],
+                'multiple' => false,
+                'expanded' => true,
+                'required' => false,
+                'placeholder' => null,
+                'label' => 'Tipo de documento',
+                'disabled' => true,
+            ])
+                ->add('documento', TextType::class, [
+                    'label' => 'Número',
+                    'disabled' => true,
+                    'required' => false,
+                ])
+                ->add('rg_ie', TextType::class, [
+                    'label' => 'RG',
+                    'disabled' => true,
+                ]);
+
+            $formData = $params['data'];
+            $formData['tp_documento'] = '1';
+            if (!is_null($this->context->customer)) {
+                $formData = $this->fillFieldsAdmin($params['data']);
+            }
+            $formBuilder->setData($formData);
+        }
+    }
+
+    public function hookActionAdminCustomersFormModifier($params)
+    {
+        $extraInputs = &$params['fields'][0]['form']['input'];
+        $extraInputs[] = [
+            'type' => 'radio',
+            'label' => 'Tipo de documento',
+            'name' => 'tp_documento',
+            'required' => true,
+            'class' => 't',
+            'values' => [
+                [
+                    'id' => 'documento_1',
+                    'value' => 1,
+                    'label' => 'CPF',
+                ],
+                [
+                    'id' => 'documento_2',
+                    'value' => 2,
+                    'label' => 'CNPJ',
+                ],
+            ],
+        ];
+
+        $extraInputs[] = [
+            'type' => 'text',
+            'label' => 'Número',
+            'name' => 'documento',
+            'required' => true,
+            'col' => '2',
+        ];
+
+        $extraInputs[] = [
+            'type' => 'text',
+            'label' => 'RG',
+            'name' => 'rg_ie',
+            'required' => false,
+            'col' => '2',
+        ];
+
+        $id_customer = (int) $params['object']->id;
+        $result = $this->searchCustomer($id_customer);
+
+        $extraValues = &$params['fields_value'];
+        $extraValues['tp_documento'] = (isset($result['tp_documento']) ? $result['tp_documento'] : 1);
+        $extraValues['documento'] = (isset($result['documento']) ? $result['documento'] : null);
+        $extraValues['rg_ie'] = (isset($result['rg_ie']) ? $result['rg_ie'] : null);
+    }
+
+    public function hookActionAdminCustomersControllerSaveBefore($params)
+    {
+        $objValidateDoc = new ValidateDocumento();
+        $documento = Tools::getValue('documento');
+        $id_customer = (int) Tools::getValue('id_customer');
+        if (!empty($documento)) {
+            if (!$objValidateDoc->validarDocumento($documento)) {
+                $params['controller']->errors[] = $this->mensagemError;
+            }
+            if ($this->checkDuplicate($documento, $id_customer) !== false) {
+                $params['controller']->errors[] = "O documento '{$documento}' já está cadastrado!";
+            }
+        } else {
+            $params['controller']->errors[] = 'O campo número do documento é obrigatório';
+        }
+    }
+
+    public function hookActionAdminCustomersControllerSaveAfter($params)
+    {
+        $id_customer = (int) Tools::getValue('id_customer');
+        $result = $this->searchCustomer($id_customer);
+        if ($result === false) {
+            $this->insertDocumento($id_customer);
+        } else {
+            $this->updateDocumento($id_customer);
+        }
+    }
+
+    public function hookAdditionalCustomerFormFields($params)
+    {
+        $format = [];
+        $tipoDocumento = (new FormField)
+            ->setName('tp_documento')
+            ->setType('radio-buttons')
+            ->setLabel('Tipo de documento')
+            ->addAvailableValue(1, 'CPF')
+            ->addAvailableValue(2, 'CNPJ')
+            ->setValue(1);
+        $format[$tipoDocumento->getName()] = $tipoDocumento;
+
+        $format['documento'] = (new FormField)
+            ->setName('documento')
+            ->setType('text')
+            ->setLabel('Número')
+            ->setRequired(true);
+
+        $format['rg_ie'] = (new FormField)
+            ->setName('rg_ie')
+            ->setType('text')
+            ->setLabel('RG')
+            ->setMaxLength(45);
+
+        $format['add_documento'] = (new FormField)
+            ->setName('add_documento')
+            ->setType('hidden')
+            ->setValue('true');
+
+        if (!is_null($this->context->customer->id)) {
+            return $this->fillFieldsFront($format);
+        }
+        return $format;
+    }
+
+    private function insertDocumento($id_customer)
+    {
+        $arrData = [
+            "documento" => preg_replace("/[^0-9]/", "", Tools::getValue('documento')),
+            "rg_ie" => substr(Tools::getValue('rg_ie'), 0, 45),
+            "tp_documento" => (int) Tools::getValue('tp_documento'),
+            "id_customer" => $id_customer,
+            "date_add" => date('Y-m-d H:i:s'),
+            "date_upd" => date('Y-m-d H:i:s'),
+        ];
+        Db::getInstance()->insert('modulo_cpf', $arrData);
+
+        $this->updateDocumentoCustomer($id_customer);
+    }
+
+    private function updateDocumento($id_customer)
+    {
+        $arrData = [
+            "documento" => preg_replace("/[^0-9]/", "", Tools::getValue('documento')),
+            "rg_ie" => substr(Tools::getValue('rg_ie'), 0, 45),
+            "tp_documento" => (int) Tools::getValue('tp_documento'),
+            "date_upd" => date('Y-m-d H:i:s'),
+        ];
+        Db::getInstance()->update('modulo_cpf', $arrData, 'id_customer = ' . (int) $id_customer);
+
+        $this->updateDocumentoCustomer($id_customer);
+    }
+
+    private function updateDocumentoCustomer($id_customer)
+    {
+        $arrData = [
+            "cpf_cnpj" => preg_replace("/[^0-9]/", "", Tools::getValue('documento')),
+            "rg_ie" => substr(Tools::getValue('rg_ie'), 0, 45),
+            "tipo" => (int) Tools::getValue('tp_documento'),
+            "date_upd" => date('Y-m-d H:i:s'),
+        ];
+        Db::getInstance()->update('customer', $arrData, 'id_customer = ' . (int) $id_customer);
+    }
+
+    public function fillFieldsAdmin(array $formData)
+    {
+        $result = $this->searchCustomer((int) $this->context->customer->id);
+        if ($result) {
+            $formData['tp_documento'] = $result['tp_documento'];
+            $formData['documento'] = $result['documento'];
+            $formData['rg_ie'] = $result['rg_ie'];
+        }
+        return $formData;
+    }
+
+    public function fillFieldsFront($format)
+    {
+        $result = $this->searchCustomer((int) $this->context->customer->id);
+        if ($result) {
+            $format['tp_documento']->setValue($result['tp_documento']);
+            $format['documento']->setValue($result['documento']);
+            $format['rg_ie']->setValue($result['rg_ie']);
+            $format['add_documento']->setValue('false');
+        }
+        return $format;
+    }
+
+    private function searchCustomer($id_customer)
+    {
+        if ($id_customer == 0) {
+            return [];
+        }
+        $db_prefix = _DB_PREFIX_;
+        $db = Db::getInstance();
+        $sql = "SELECT * FROM `{$db_prefix}modulo_cpf` WHERE id_customer = {$id_customer}";
+        return $db->getRow($sql);
+    }
+
+    public function validarDocumento($documento)
+    {
+        $objValidateDoc = new ValidateDocumento();
+        if (!$objValidateDoc->validarDocumento($documento)) {
+            throw new Exception($this->mensagemError);
+        }
+        $id_customer = (is_null($this->context->customer->id) ? null : $this->context->customer->id);
+        if ($this->checkDuplicate($documento, $id_customer) !== false) {
+            throw new Exception('O documento informado já está cadastrado!');
+        }
+    }
+
+    public function checkDuplicate($documento, $id_customer = 0)
+    {
+        $db_prefix = _DB_PREFIX_;
+        $db = Db::getInstance();
+        $doc = preg_replace("/[^0-9]/", "", $documento);
+        $sql = "SELECT * FROM `{$db_prefix}modulo_cpf` WHERE `documento` = '{$doc}'";
+        if ($id_customer > 0) {
+            $sql .= " AND id_customer != {$id_customer}";
+        }
+        $result = $db->getRow($sql);
+        return $result;
+    }
+
+    private function alteraTabela()
+    {
+        $db = Db::getInstance();
+
+        $sql = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '" . _DB_PREFIX_ . "customer' AND column_name = 'tipo' AND table_schema = '" . _DB_NAME_ . "'";
+        $dados = $db->getRow($sql);
+        if (!$dados) {
+            $sql = "ALTER TABLE " . _DB_PREFIX_ . "customer ADD tipo varchar(2) DEFAULT ' ';";
+            $db->Execute($sql);
+        }
+
+        $sql = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '" . _DB_PREFIX_ . "customer' AND column_name = 'cpf_cnpj' AND table_schema = '" . _DB_NAME_ . "'";
+        $dados = $db->getRow($sql);
+        if (!$dados) {
+            $sql = "ALTER TABLE " . _DB_PREFIX_ . "customer ADD cpf_cnpj varchar(20) DEFAULT ' ';";
+            $db->Execute($sql);
+        }
+
+        $sql = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '" . _DB_PREFIX_ . "customer' AND column_name = 'rg_ie' AND table_schema = '" . _DB_NAME_ . "'";
+        $dados = $db->getRow($sql);
+        if (!$dados) {
+            $sql = "ALTER TABLE " . _DB_PREFIX_ . "customer ADD rg_ie varchar(45) DEFAULT ' ';";
+            $db->Execute($sql);
+        }
+
+        return true;
+    }
+}
